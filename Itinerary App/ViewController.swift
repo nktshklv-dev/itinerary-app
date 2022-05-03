f044f110e3c56a14f3980cca6eb4b581ece1dd8f
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-05-04 00:42:05 +0300

    Initial Commit

---
+++ Itinerary App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Itinerary App
+//
+//  Created by Nikita  on 5/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
