d93bcb1621f2a1f3d3ee07a0b55ae580a02ae9bf
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-03 13:54:21 +0300

    created addTripVC

---
+++ Itinerary App/AddTripVC/AddTripVc.swift
@@ -0,0 +1,29 @@
+//
+//  AddTripVc.swift
+//  Itinerary App
+//
+//  Created by Nikita  on 6/3/22.
+//
+
+import UIKit
+
+class AddTripVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
